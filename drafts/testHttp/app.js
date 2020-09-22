497617e71ee9ebae860b7be91de8ea24349af4a4
vbasakov <vbasakov@gmail.com>
2020-09-22 08:36:16 +0300

    http json request records

--- drafts/testHttp/app.js
+++ drafts/testHttp/app.js
@@ -2,22 +2,48 @@
     'use strict';
 
     angular.module("angModule", [])
-        .controller("tableController", tableController())
-        .constant("jsonApiPath","http://127.0.0.1:8080/json");
+        .controller("TableController", TableController)
+        .service("RecordsService", RecordsService)
+        .constant("jsonApiPath", "http://127.0.0.1:8080/json");
 
-    function tableController() {
-        return function ($scope) {
-            var ctrl = this;
+    TableController.$inject = ['RecordsService']
 
-            ctrl.list = [{
-                "operDate": "20.06.2020 23:24:21",
-                "sum": 1180.0,
-                "description": "Delivery Club"
-            }, {"operDate": "25.06.2020 23:09:09", "sum": 99.0, "description": "Kega"}];
+    function TableController(RecordsService) {
+        var ctrl = this;
 
-            $scope.nameClick = function (item) {
-                item.exposed = !item.exposed;
-            }
+        var promise = RecordsService.getRecords();
+        promise.then(function (response) {
+            ctrl.list = response.data;
+        }).catch(function (error) {
+            console.log("Something wrong...");
+            console.log(error);
+        });
+
+        // ctrl.list = [{
+        //     "operDate": "20.06.2020 23:24:21",
+        //     "sum": 1180.0,
+        //     "description": "Delivery Club"
+        // }, {"operDate": "25.06.2020 23:09:09", "sum": 99.0, "description": "Kega"}];
+
+        ctrl.nameClick = function (item) {
+            item.exposed = !item.exposed;
+        }
+
+    }
+
+    RecordsService.$inject = ['$http', 'jsonApiPath'];
+
+    function RecordsService($http, jsonApiPath) {
+        var service = this;
+
+        service.getRecords = function () {
+            console.log("getRecords ...");
+            console.log(jsonApiPath + "/");
+            var response = $http({
+                method: "GET",
+                url: (jsonApiPath + "/")
+            });
+            return response;
         };
     }
 
