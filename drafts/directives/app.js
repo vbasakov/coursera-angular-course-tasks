5ec0fcbffe645dff93d94aa25860d9b9c29200de
Vadim Basakov <vbasakov@gmail.com>
2020-09-22 22:33:06 +0300

    angular directives

---
+++ drafts/directives/app.js
@@ -0,0 +1,32 @@
+(function () {
+    'use strict';
+
+
+    angular.module("angModule", [])
+        .controller("TableController", TableController)
+        .directive("dir1", myDirective);
+
+    function myDirective() {
+        var ddo = {
+            templateUrl: 'dir1.html',
+            scope: {
+                prop1: '=',   // two-way binding        without {{ }}
+                prop2: '@',    // as string binding     {{ }} can be, but unnessessary
+                prop3: '<'    // one-way binding        without {{ }}
+            }
+        };
+        return ddo;
+    }
+
+
+    function TableController() {
+        var ctrl = this;
+
+        ctrl.prop1 = "Property 1";
+        ctrl.prop2 = "Property 2";
+        ctrl.prop3 = "Property 3";
+
+    }
+
+
+})();
