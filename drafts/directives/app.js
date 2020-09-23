b624916482136c644b713fe13639093b0ea70f19
Vadim Basakov <vbasakov@gmail.com>
2020-09-23 08:48:07 +0300

    angular directives. Method calls propogation

--- drafts/directives/app.js
+++ drafts/directives/app.js
@@ -10,9 +10,10 @@
         var ddo = {
             templateUrl: 'dir1.html',
             scope: {
-                prop1: '=',   // two-way binding        without {{ }}
+                prop1: '=',    // two-way binding        without {{ }}
                 prop2: '@',    // as string binding     {{ }} can be, but unnessessary
-                prop3: '<'    // one-way binding        without {{ }}
+                prop3: '<',    // one-way binding        without {{ }}
+                innerMethod: '&outerMethod'
             }
         };
         return ddo;
@@ -25,7 +26,9 @@
         ctrl.prop1 = "Property 1";
         ctrl.prop2 = "Property 2";
         ctrl.prop3 = "Property 3";
-
+        ctrl.someMethod = function (str) {
+            ctrl.prop4 = str;
+        }
     }
 
 
