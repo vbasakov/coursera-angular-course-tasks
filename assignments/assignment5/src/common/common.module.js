8e3bc79c5e614829a403e3c7fbcd528b2ba3b612
vbasakov <vbasakov@gmail.com>
2020-10-14 22:43:54 +0300

    Assignment 5 server setup has been completed

--- assignments/assignment5/src/common/common.module.js
+++ assignments/assignment5/src/common/common.module.js
@@ -2,7 +2,7 @@
     "use strict";
 
     angular.module('common', [])
-        .constant('ApiPath', 'https://ychaikin-course5.herokuapp.com')
+        .constant('ApiPath', 'https://coursera-angular-17.herokuapp.com')
         .config(config);
 
     config.$inject = ['$httpProvider'];
