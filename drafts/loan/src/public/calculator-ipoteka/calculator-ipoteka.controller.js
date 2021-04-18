26a826cad5b5aaeb43be5c9156e35016f03da47f
vbasakov <vbasakov@gmail.com>
2021-04-18 22:51:35 +0400

    ipo calc form

--- drafts/loan/src/public/calculator-ipoteka/calculator-ipoteka.controller.js
+++ drafts/loan/src/public/calculator-ipoteka/calculator-ipoteka.controller.js
@@ -10,5 +10,22 @@
 
         };
 
+
+        ipoCtrl.loanRecords = [{
+            num: 1,
+            date: 'Апрель, 2021',
+            sum: 37592.72,
+            main: 3217.72,
+            procents: 34375.00,
+            rest: 2496782.28
+        }, {
+            num: 2,
+            date: 'Май, 2021',
+            sum: 39000,
+            main: 3333,
+            procents: 34375.77,
+            rest: 100500
+        }]
+
     }
 })();
